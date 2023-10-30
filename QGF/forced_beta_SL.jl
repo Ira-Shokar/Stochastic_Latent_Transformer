d7a604bd2272011220f0f15794dae4119498b7fb
Ira Shokar <ira.shokar@gmail.com>
2023-10-30 14:53:20 +0000

    add julia requirements

--- QGF/forced_beta_SL.jl
+++ QGF/forced_beta_SL.jl
@@ -18,33 +18,75 @@
 #using Pkg
 #Pkg.activate(".")
 
-using GeophysicalFlows, CUDA, Random, Printf#, Metal
+using GeophysicalFlows, CUDA, Random, Printf #, Metal
 using Statistics: mean
 using LinearAlgebra: ldiv!
 import FourierFlows as FF
 
-dev = GPU();     # Device (CPU/GPU)
-
-if dev==CPU(); Random.seed!(0); else; CUDA.seed!(0); end;
-
-              n = 256                   # 2D resolution: n² grid points
-             dt = 4e-2                  # timestep
-          t_max = 1000                  # integration time
-  save_substeps = 2500                  # number of timesteps after which output is saved
-         nsteps = t_max*save_substeps   # total number of timesteps
-
               L = 2π                    # domain size
+              n = 256                   # 2D resolution: n² grid points
+
               β = 0.9                   # planetary PV gradient
               μ = 4e-4                  # bottom drag
+
              nν = 8                     # hyperviscosity order
               ν = (n/3)^(-nν*2)         # hyperviscosity coefficent
 
-              ε = 1e-6                  # forcing energy input rate
- 
+             dt = 4e-2                  # timestep
+          t_max = 500                   # integration time
+  save_substeps = 2500                  # number of timesteps after which output is saved
+         nsteps = t_max*save_substeps   # total number of timesteps
+
+      ε = 1e-6          # forcing energy input rate
     k_f = 16.0 * 2π/L   # the forcing wavenumber, `k_f`, for a spectrum that is a ring in wavenumber space
 k_width = 1.5  * 2π/L   # the width of the forcing spectrum, `δ_f`
 
-function forcingspectrum(grid::AbstractGrid)
+dev = GPU();  # Device (CPU/GPU)
+
+if dev==CPU(); Random.seed!(0); else; CUDA.seed!(0); end;
+
+prob = SingleLayerQG.Problem(
+    dev; nx=n,ny=n, Lx=L, Ly = L, β=β, μ=μ, ν=ν, nν = nν, dt,
+    stepper="FilteredRK4", calcF=calcF!, stochastic=true, aliased_fraction = 1/3
+    );
+
+SingleLayerQG.set_q!(prob, device_array(dev)(zeros(prob.grid.nx, prob.grid.ny)));
+
+E = Diagnostic(SingleLayerQG.energy, prob; nsteps, freq=save_substeps)
+Z = Diagnostic(SingleLayerQG.enstrophy, prob; nsteps, freq=save_substeps)
+diags = [E, Z]; # A list of Diagnostics types passed to "stepforward!" will  be updated every timestep.
+
+filename = "singlelayerqg_forcedbeta.jld2";
+
+if isfile(filename); rm(filename); end
+
+output = Output(prob, filename, (:u, get_u), (:q, get_q), (:ψ, get_ψ))
+
+saveproblem(output)
+ saveoutput(output)
+
+startwalltime = time()
+
+while prob.clock.step <= nsteps
+
+  stepforward!(prob, diags, save_substeps)
+  SingleLayerQG.updatevars!(prob)
+
+  if prob.clock.step % save_substeps == 0
+    log = @sprintf("step: %04d, t: %d, E: %.3e, Q: %.3e, walltime: %.2f min",
+    prob.clock.step, prob.clock.t, E.data[E.i], Z.data[Z.i], (time()-startwalltime)/60)
+    println(log)
+    saveoutput(output)
+  end
+end
+
+savediagnostic(E, "energy"   , output.path)
+savediagnostic(Z, "enstrophy", output.path)
+
+### FUNCTIONS #####################################################################################
+
+
+function forcingspectrum(ε, k_f, k_width, grid::AbstractGrid)
 
   K = @. sqrt(grid.Krsq)            # a 2D array with the total wavenumber
 
@@ -56,35 +98,13 @@ function forcingspectrum(grid::AbstractGrid)
   return forcing_spectrum
 end
 
+forcing_spectrum = forcingspectrum(ε, k_f, k_width, FF.TwoDGrid(dev; nx=n, Lx=L))
+
 function calcF!(Fh, sol, t, clock, vars, params, grid)
   T = eltype(grid)
   @. Fh = sqrt(forcing_spectrum) * cis(2π * rand(T)) / sqrt(clock.dt)
   return nothing
 end;
-
-forcing_spectrum = forcingspectrum(FF.TwoDGrid(dev; nx=n, Lx=L))
-
-prob = SingleLayerQG.Problem(
-    dev; nx=n,ny=n, Lx=L, Ly = L, β=β, μ=μ, ν=ν, nν = nν, dt,
-    stepper="FilteredRK4", calcF=calcF!, stochastic=true, aliased_fraction = 1/3);
-
-sol, clock, vars, params, grid = prob.sol, prob.clock, prob.vars, prob.params, prob.grid
-x,  y  = grid.x,  grid.y
-Lx, Ly = grid.Lx, grid.Ly;
-
-SingleLayerQG.set_q!(prob, device_array(dev)(zeros(grid.nx, grid.ny)));
-
-E = Diagnostic(SingleLayerQG.energy, prob; nsteps, freq=save_substeps)
-Z = Diagnostic(SingleLayerQG.enstrophy, prob; nsteps, freq=save_substeps)
-diags = [E, Z]; # A list of Diagnostics types passed to "stepforward!" will  be updated every timestep.
-
-filepath = "."
-plotpath = "./plots_forcedbetaturb"
-plotname = "snapshots"
-filename = joinpath(filepath, "singlelayerqg_forcedbeta.jld2");
-
-if isfile(filename); rm(filename); end
-if !isdir(plotpath); mkdir(plotpath); end;
 
 get_q(prob) = Array(prob.vars.q)
 get_ψ(prob) = Array(prob.vars.ψ)
@@ -96,26 +116,3 @@ function get_u(prob)
   ldiv!(prob.vars.u, prob.params.rfftplan, prob.vars.uh)
   return Array(prob.vars.u)
 end
-    
-output = Output(prob, filename, (:u, get_u), (:q, get_q), (:ψ, get_ψ))
-
-saveproblem(output)
- saveoutput(output)
-
-startwalltime = time()
-
-while clock.step <= nsteps
-
-  stepforward!(prob, diags, save_substeps)
-  SingleLayerQG.updatevars!(prob)
-
-  if clock.step % save_substeps == 0
-    log = @sprintf("step: %04d, t: %d, E: %.3e, Q: %.3e, walltime: %.2f min",
-    clock.step, clock.t, E.data[E.i], Z.data[Z.i], (time()-startwalltime)/60)
-    println(log)
-    saveoutput(output)
-  end
-end
-
-savediagnostic(E, "energy", output.path)
-savediagnostic(Z, "enstrophy", output.path)
